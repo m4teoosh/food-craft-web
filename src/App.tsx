4c0f0696f3e713a1a1e29dea12451cf197d5bce4
m4teoosh <mateusz.kadlubowski99@gmail.com>
2022-09-08 20:02:08 +0200

    config shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import NewIngredient from "./components/new-ingredient/container/NewIngredient";
-import axios from "axios";
 import Recipe from "./components/recipe/Recipe";
 
 
